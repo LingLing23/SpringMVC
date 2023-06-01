bdd01d28298c876eeb5440a8218152f9d2454225
LingLing23 <ryoung6920@gmail.com>
2023-06-01 17:53:01 +0900

    06/01 17:52

--- SpringMVC/src/main/java/com/itwillbs/persistence/MemberDAO.java
+++ SpringMVC/src/main/java/com/itwillbs/persistence/MemberDAO.java
@@ -1,6 +1,6 @@
 package com.itwillbs.persistence;
 
-
+import com.itwillbs.domain.MemberVO;
 
 //persistence (영속성) -> DB관련 처리
 public interface MemberDAO {
@@ -8,5 +8,24 @@ public interface MemberDAO {
 	
 	// DB 시간정보 조회
 	public String getTime();
+	
+	
+	// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ CRUD ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+	// C - 회원정보 가입
+	public void insertMember(MemberVO vo);
+	
+	// R - 로그인 (회원정보 조회)
+	// 방법1 )  사실상 이걸 써야하는게 맞고 젤 많이 사용함
+	public MemberVO loginMemeber(MemberVO vo);
+	
+	// R - 로그인 (오버로딩)
+	// 방법2 )
+	public MemberVO loginMemeber(String id, String pw);
+	
+	// U - 회원정보 수정
+	public Integer updateMember(MemberVO uvo);
+	
+	// D - 회원정보 삭제
+	public Integer deleteMember(MemberVO dvo); 
 	
 }
